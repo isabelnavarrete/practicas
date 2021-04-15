9be2795f86fd6af0bf26dc6259627901a6be5f9b
isabelnavarrete <82452855+isabelnavarrete@users.noreply.github.com>
2021-04-15 14:55:06 +0200

    Add files via upload

---
+++ ejemploListasDiccionarios/ejemploListasDiccionarios/run.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def holamundo():
+    lista = ['Alvaro', 'Bernardo', 'Cecilia', 'Daniel']
+    diccionario = {'nombre': 'marta', 'edad': 20, 'curso': 'Java'}
+    mezclado = {'nombres': ['Alvaro', 'Bernardo', 'Cecilia', 'Daniel'], 'edades': [23, 45, 34, 23], 'cursos': ['Java', 'Python', 'PHP', 'CSS']}
+    return render_template('index.html', lista=lista, diccionario=diccionario, mezclado=mezclado)
+
+
