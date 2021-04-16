2d5b70cb748ea448db3c35595c801d7cfc80236a
isabelnavarrete <82452855+isabelnavarrete@users.noreply.github.com>
2021-04-16 09:07:56 +0200

    Add files via upload

--- ejemploListasDiccionarios/run.py
+++ ejemploListasDiccionarios/run.py
@@ -2,17 +2,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-"""listas"""
-@app.route('/<string:slug>/')
-def holamundo (slug):
+@app.route('/')
+def holamundo():
     lista = ['Alvaro', 'Bernardo', 'Cecilia', 'Daniel']
     diccionario = {'nombre': 'marta', 'edad': 20, 'curso': 'Java'}
     mezclado = {'nombres': ['Alvaro', 'Bernardo', 'Cecilia', 'Daniel'], 'edades': [23, 45, 34, 23], 'cursos': ['Java', 'Python', 'PHP', 'CSS']}
-    return render_template('index.html', lista=lista, diccionario=diccionario, mezclado=mezclado, slug=slug)
+    return render_template('index.html', lista=lista, diccionario=diccionario, mezclado=mezclado)
+
+app.run(host="0.0.0.0", port=8080, debug=True)
 
 
-"""diccionario"""
-""""@app.route('/view')
-def diccionario ():
-    diccionario = {'nombre': 'marta', 'edad': 20, 'curso': 'Java'}
-    return render_template('post_view.html', diccionario=diccionario)"""
