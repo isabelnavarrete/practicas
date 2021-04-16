b64b74dc8a94150effa964b0fc3563bf1243f141
isabelnavarrete <82452855+isabelnavarrete@users.noreply.github.com>
2021-04-16 11:12:01 +0200

    Add files via upload

--- ejemploListasDiccionarios/run.py
+++ ejemploListasDiccionarios/run.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def holamundo():
-    lista = ['Alvaro', 'Bernardo', 'Cecilia', 'Daniel']
+    lista = [23,45,34]
     diccionario = {'nombre': 'marta', 'edad': 20, 'curso': 'Java'}
     mezclado = {'nombres': ['Alvaro', 'Bernardo', 'Cecilia', 'Daniel'], 'edades': [23, 45, 34, 23], 'cursos': ['Java', 'Python', 'PHP', 'CSS']}
-    return render_template('index.html', lista=lista, diccionario=diccionario, mezclado=mezclado)
+    mezcladoLD= [{'nombre':'marta', 'edad':24, 'curso':'Java'}, {'nombre':'fernando', 'edad':21, 'curso':'HTMl'}, {'nombre':'Victoria', 'edad':24, 'curso':'PHP'}, {'nombre':'Laura', 'edad':23, 'curso':'Java'}, {'nombre':'Jesus', 'edad':80, 'curso':'PHP'}]
+    return render_template('index.html', lista=lista, diccionario=diccionario, mezclado=mezclado, mezcladoLD=mezcladoLD)
+
 
 app.run(host="0.0.0.0", port=8080, debug=True)
-
 
